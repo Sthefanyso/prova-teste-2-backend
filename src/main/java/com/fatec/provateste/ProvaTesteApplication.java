4950158f8ca128f7075232d0faddf23484ff1544
Sthefanyso <sthefany.silva3@fatec.sp.gov.br>
2023-06-19 08:53:52 -0300

    first commit

---
+++ src/main/java/com/fatec/provateste/ProvaTesteApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.provateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/provateste/ProvaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.provateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
